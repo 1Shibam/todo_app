e8339720dbcb00606f6a61e611848c8954fe1808
1Shibam <hehe.shibam@gmail.com>
2025-01-14 16:50:40 +0530

    making a todo app for practice, first commit!!

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/screens/todo_app_home_screen.dart';
+
+void main() => runApp(const ProviderScope(child: TodoApp()));
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData.dark(),
+      home: const TodoAppHomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/todo_app_home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodoAppHomeScreen extends StatelessWidget {
+  const TodoAppHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello Bitch San!!'),
+      ),
+    );
+  }
+}
