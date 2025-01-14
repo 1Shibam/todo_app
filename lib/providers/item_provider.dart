972bfb4b9421c084ffe9bac30801fe5139cce050
1Shibam <hehe.shibam@gmail.com>
2025-01-14 21:43:55 +0530

    bye bye

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -1,35 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/models/items.dart';
 
-final itemProvider = StateNotifierProvider<ItemNotifier, List<Items>>((ref){
+final itemProvider = StateNotifierProvider<ItemNotifier, List<Items>>((ref) {
   return ItemNotifier();
-
 });
 
-//the class below is extending state notifier i.e,  
-class ItemNotifier extends StateNotifier<List<Items>>{ // state notifier always needs a state here its list of Items class  
+//the class below is extending state notifier i.e,
+class ItemNotifier extends StateNotifier<List<Items>> {
+  // state notifier always needs a state here its list of Items class
   //
-  ItemNotifier(): super([]);
+  ItemNotifier() : super([]);
 
   //add item to the list
-  void addItem(){
-
-  }
-
-  //delete data in the list
-  void deleteItem(){
-
-  }
-
-  //update data in the list
-  void updateItem(){
-
-  }
-
-  //read data present in the list
-  void lookItem(){
+  void addItem(String itemName) {
+    final item = Items(id: DateTime.now().toString(), name: itemName);
+    state.add(item);
+    state = state.toList();
     
   }
 
+  //delete data in the list
+  void deleteItem() {}
 
-} 
+  //update data in the list
+  void updateItem() {}
+
+  //read data present in the list
+  void lookItem() {}
+}
--- lib/screens/todo_app_home_screen.dart
+++ lib/screens/todo_app_home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/providers/item_provider.dart';
 
-class TodoAppHomeScreen extends StatelessWidget {
+class TodoAppHomeScreen extends ConsumerWidget {
   const TodoAppHomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+  Widget build(BuildContext context, ref) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(itemProvider.notifier).addItem('newItem');
+        },
+        child: const Text('Add task'),
+      ),
+      body: const Center(
         child: Text('Hello Bitch San!!'),
       ),
     );
