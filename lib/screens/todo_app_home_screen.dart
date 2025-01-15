77c4a804ef0fd3ec7190d321b5a4100b0514a03b
1Shibam <hehe.shibam@gmail.com>
2025-01-15 13:06:43 +0530

    implementation in UI

--- lib/screens/todo_app_home_screen.dart
+++ lib/screens/todo_app_home_screen.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/methods/show_name_dialog.dart';
 import 'package:todo_app/providers/item_provider.dart';
-
-
 
 class TodoAppHomeScreen extends ConsumerWidget {
   const TodoAppHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
+    // final itemName = ref.watch(itemNameProvider);
+    // final itemID = ref.watch(itemIDProvider);
     final dataItems = ref.watch(itemProvider);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          ref.read(itemProvider.notifier).addItem('nigga');
+          showNameDialog(context, ref);
+          
         },
         child: const Text('+'),
       ),
@@ -27,7 +29,8 @@ class TodoAppHomeScreen extends ConsumerWidget {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    ListTile(title: Text('${dataItems[index].name}-$index')),
+                    ListTile(title: Text(dataItems[index].name),
+                    subtitle: Text("${(dataItems[index].id)}"),),
                   ],
                 );
               }),
