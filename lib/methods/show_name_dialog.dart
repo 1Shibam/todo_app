dab86a40f7f4ad36158be24bb44ce6b725f8e580
1Shibam <hehe.shibam@gmail.com>
2025-01-15 13:05:39 +0530

    fixed dialog popup

--- lib/methods/show_name_dialog.dart
+++ lib/methods/show_name_dialog.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/providers/item_provider.dart';
 
 final itemNameProvider = StateProvider<String>((ref) => '');
-final textController =
+final itemIDProvider = StateProvider<int>((ref) => 0);
+final nameTextController =
+    Provider<TextEditingController>((ref) => TextEditingController());
+final idTextController =
     Provider<TextEditingController>((ref) => TextEditingController());
 
-void _showNameDialog(BuildContext context, WidgetRef ref) {
-  final controller = ref.watch(textController);
+void showNameDialog(BuildContext context, WidgetRef ref) {
+  final nameController = ref.watch(nameTextController);
+  final idController = ref.watch(idTextController);
 
   showDialog(
     context: context,
     builder: (context) {
       return AlertDialog(
-        title: const Text('Enter Item Name'),
-        content: TextField(
-          controller: controller,
-          decoration: const InputDecoration(hintText: 'Item name'),
+        title: const Text('Enter Item Details'),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                    hintText: 'Item name', border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextField(
+                controller: idController,
+                decoration: const InputDecoration(
+                    hintText: 'Item Id', border: OutlineInputBorder()),
+              ),
+            ],
+          ),
         ),
         actions: [
           TextButton(
@@ -28,7 +49,9 @@ void _showNameDialog(BuildContext context, WidgetRef ref) {
           ElevatedButton(
             onPressed: () {
               // Save the input to Riverpod state
-              ref.read(itemNameProvider.notifier).state = controller.text;
+              ref
+                  .read(itemProvider.notifier)
+                  .addItem(nameController.text, int.parse(idController.text));
               Navigator.of(context).pop(); // Close dialog after saving
             },
             child: const Text('Add'),
