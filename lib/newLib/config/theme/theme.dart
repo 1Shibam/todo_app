0e3c5b978472b87e5e1e04cd24bc7e078b9d55ef
1Shibam <hehe.shibam@gmail.com>
2025-01-16 00:33:44 +0530

    lol

--- lib/newLib/config/theme/theme.dart
+++ lib/newLib/config/theme/theme.dart
@@ -1,5 +1,13 @@
+//fonts
+
+import 'package:flutter/material.dart';
+
 const String font = 'Classica-Book';
 const String fontBold = 'Classica-Bold';
-class Theme {
-  
-}
+
+//colors
+const background = Color(0xff121212);
+const primaryText = Color(0xffE0E0E0);
+const accent = Color(0xffBB86FC);
+const secondaryText = Color(0xff757575);
+const buttons = Color(0xff03DAC6);
