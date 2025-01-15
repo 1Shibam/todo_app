657d14c3541ed073145f45edc1bcb95bbc14af76
1Shibam <hehe.shibam@gmail.com>
2025-01-15 12:08:59 +0530

    add items feature

--- lib/screens/todo_app_home_screen.dart
+++ lib/screens/todo_app_home_screen.dart
@@ -2,23 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/providers/item_provider.dart';
 
-
 class TodoAppHomeScreen extends ConsumerWidget {
   const TodoAppHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
+    final dataItems = ref.watch(itemProvider);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           ref.read(itemProvider.notifier).addItem('newItem');
         },
-        child: const Text('Add task'),
+        child: const Text('+'),
       ),
-      body: const Center(
-        child: Text('Hello Bitch San!!'),
+      body: ListView.builder(
+        
+        itemCount: dataItems.length,
+        itemBuilder: (context, index) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(title: Text(dataItems[index].name)),
+            ],
+          );
+        },
       ),
     );
   }
 }
-
