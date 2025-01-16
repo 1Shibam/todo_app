89e2e9fbfdb76af2e0488c1a251b77bfa936cb10
1Shibam <hehe.shibam@gmail.com>
2025-01-17 00:52:26 +0530

    created crud methods

---
+++ lib/newLib/providers/Task Providers/task_list_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/newLib/models/task_data_model.dart';
+
+class TaskListNotifier extends StateNotifier<List<TaskDataModel>> {
+  TaskListNotifier() : super([]);
+
+  // Add a task
+  void addTask(TaskDataModel task) {
+    state = [...state, task];
+  }
+
+  // Toggle the 'isDone' status of a task
+  void toggleTaskStatus(String title) {
+    state = state.map((task) {
+      if (task.title == title) {
+        return task.copyWith(isDone: !(task.isDone ?? false));
+      }
+      return task;
+    }).toList();
+  }
+
+  // Delete a task by title
+  void deleteTask(String title) {
+    state = state.where((task) => task.title != title).toList();
+  }
+
+  // Clear all deleted tasks
+  void clearDeletedTasks() {
+    state = state.where((task) => !(task.isDeleted ?? false)).toList();
+  }
+
+  // Mark a task as deleted
+  void markTaskAsDeleted(String title) {
+    state = state.map((task) {
+      if (task.title == title) {
+        return task.copyWith(isDeleted: true);
+      }
+      return task;
+    }).toList();
+  }
+}
+
+// Create the TaskListProvider
+final taskListProvider = StateNotifierProvider<TaskListNotifier, List<TaskDataModel>>(
+  (ref) => TaskListNotifier(),
+);
+
+final singleTaskProvider = StateProvider<TaskDataModel>(
+  (ref) => TaskDataModel(title: '', isDone: false, isDeleted: false),
+);
--- lib/newLib/screens/tasks_screen.dart
+++ lib/newLib/screens/tasks_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/newLib/models/task_data_model.dart';
+import 'package:todo_app/newLib/providers/Task%20Providers/task_list_provider.dart';
 import 'package:todo_app/newLib/widgets/app_text_styles.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -7,12 +9,14 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<TaskDataModel> data = [
+    /*Testing list
+
+     List<TaskDataModel> data = [
       TaskDataModel(title: 'Home work'),
-      TaskDataModel(title: 'Jerk Off'),
+       TaskDataModel(title: 'Jerk Off'),
       TaskDataModel(title: 'More Home work'),
       TaskDataModel(title: 'A lot of Jerk Off'),
-    ];
+    ];*/
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -21,29 +25,49 @@ class TasksScreen extends StatelessWidget {
           style: AppTextStyles.bold(fontSize: 32),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                var task = data[index];
-                return ListTile(
-                  trailing: Checkbox(
-                    value: task.isDone,
-                    onChanged: (value) {
-                      task.copyWith(isDone: value);
-                    },
-                  ),
-                  title: Text(
-                    task.title,
-                    style: AppTextStyles.normal(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      floatingActionButton: Consumer(
+        builder: (context, ref, child) {
+          return FloatingActionButton(
+            onPressed: () {
+              final newTask = TaskDataModel(title: 'New Task');
+              ref.read(taskListProvider.notifier).addTask(newTask);
+            },
+            backgroundColor: Colors.grey,
+            child: const Icon(Icons.add_task),
+          );
+        },
+      ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final tasks = ref.watch(taskListProvider);
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+                    return ListTile(
+                      trailing: Checkbox(
+                        value: task.isDone,
+                        onChanged: (value) {
+                          ref
+                              .read(taskListProvider.notifier)
+                              .toggleTaskStatus(task.title);
+                        },
+                      ),
+                      title: Text(
+                        task.title,
+                        style: AppTextStyles.normal(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
