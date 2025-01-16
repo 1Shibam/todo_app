1ed6068f71563dcde6d97fb6638f55308fec7896
1Shibam <hehe.shibam@gmail.com>
2025-01-17 01:08:11 +0530

    add consumer widget for displaying tasks

---
+++ lib/newLib/widgets/task_list_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/newLib/providers/Task%20Providers/task_list_provider.dart';
+import 'package:todo_app/newLib/widgets/app_text_styles.dart';
+
+class TaskListWidget extends StatelessWidget {
+  const TaskListWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        final tasks = ref.watch(taskListProvider);
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  final task = tasks[index];
+                  return ListTile(
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Checkbox(
+                          value: task.isDone,
+                          onChanged: (value) {
+                            ref
+                                .read(taskListProvider.notifier)
+                                .toggleTaskStatus(task.title);
+                          },
+                        ),
+                        IconButton(onPressed: (){
+                          ref.read(taskListProvider.notifier).deleteTask(task.title);
+                        }, icon: const Icon(Icons.delete))
+                      ],
+                    ),
+                    title: Text(
+                      task.title,
+                      style: AppTextStyles.normal(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
