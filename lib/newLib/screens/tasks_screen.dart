a40431b78846b415faf69725429bad0533f49121
1Shibam <hehe.shibam@gmail.com>
2025-01-17 00:00:52 +0530

    add text style widget to reduce redundancy

--- lib/newLib/screens/tasks_screen.dart
+++ lib/newLib/screens/tasks_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/newLib/widgets/app_text_styles.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ,
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello world!',
+          style: AppTextStyles.normal(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/newLib/widgets/app_text_styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/themes/fonts.dart';
+
+class AppTextStyles {
+  // Normal font style
+  static TextStyle normal({
+    double fontSize = 14.0,
+    Color color = Colors.black,
+  }) {
+    return TextStyle(
+      fontFamily: font,
+      fontSize: fontSize,
+      color: color,
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  // Bold font style
+  static TextStyle bold({
+    double fontSize = 16.0,
+    Color color = Colors.black,
+  }) {
+    return TextStyle(
+      fontFamily: fontBold,
+      fontSize: fontSize,
+      color: color,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
