063ffa8e559a997494573af3efcaf0c3504d0528
1Shibam <hehe.shibam@gmail.com>
2025-01-16 23:20:55 +0530

    minor project changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todo_app/newLib/app/todo_app.dart';
+import 'package:todo_app/newLib/screens/tasks_screen.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
-  runApp(const ProviderScope(child: TodoApp()));
+  runApp(const ProviderScope(child: TasksScreen()));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app/newLib/screens/tasks_screen.dart';
 
-import 'package:todo_app/newLib/screens/todo_home_screen.dart';
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TodoHomeScreen());
+    await tester.pumpWidget(const TasksScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
