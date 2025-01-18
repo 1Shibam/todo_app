982ed760ee59ff7100746f7b937c5181ced694da
1Shibam <hehe.shibam@gmail.com>
2025-01-18 14:29:48 +0530

    update exports

--- lib/widgets/widgets_export.dart
+++ lib/widgets/widgets_export.dart
@@ -1,3 +1,4 @@
 export 'app widgets/app_text_styles.dart';
 export 'app widgets/task_list_widget.dart';
 export 'app widgets/create_task_details_widget.dart';
+export 'app widgets/todays_quote.dart';
