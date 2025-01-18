d3663c16fa6f55e662da1d06f93df884a0df314d
1Shibam <hehe.shibam@gmail.com>
2025-01-18 14:23:07 +0530

    add quote of the day feature, update once a day

--- lib/providers/quote_provider/random_quote.dart
+++ lib/providers/quote_provider/random_quote.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:todo_app/models/data%20moodels/quote_data_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/providers/provider_exports.dart';
+import 'package:todo_app/models/models_exports.dart';
 
 // String finalQuote = '';
 // generateQuote() async {
@@ -10,15 +11,32 @@ import 'package:todo_app/providers/provider_exports.dart';
 //   print(response.body);
 // }
 
-final quoteProvider = FutureProvider<QuoteDataModel>((ref) async {
-  const String url = 'https://api.adviceslip.com/advice';
-  final response = await http.get(Uri.parse(url));
+final quoteProvider = FutureProvider<String>((ref) async {
+  final prefs = await SharedPreferences.getInstance();
+  final currentDate = DateTime.now().toIso8601String().split('T').first; // Get current date in yyyy-MM-dd format
+  final storedDate = prefs.getString('quoteDate');
+  final storedQuote = prefs.getString('quote');
 
-  if (response.statusCode == 200) {
-    // Parse the response to get the advice text
-    final data = json.decode(response.body);
-    return QuoteDataModel.fromJson(data);
+  if (storedDate == currentDate && storedQuote != null) {
+    // If quote is already stored for today, return the stored quote
+    return storedQuote;
   } else {
-    throw Exception('Failed to load advice');
+    // If it's a new day, fetch a new quote
+    const String uri = 'https://api.adviceslip.com/advice';
+    final response = await http.get(Uri.parse(uri));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final quote = QuoteDataModel.fromJson(data); // Use the Quote model here
+
+      // Store the new quote and the current date
+      await prefs.setString('quote', quote.advice);
+      await prefs.setString('quoteDate', currentDate);
+
+      return quote.advice;
+    } else {
+      throw Exception('Failed to load advice');
+    }
   }
 });
+
