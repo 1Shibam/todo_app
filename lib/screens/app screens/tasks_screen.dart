422e9bd31c913d6e964ac9d433f47ed15d8ff016
1Shibam <hehe.shibam@gmail.com>
2025-01-18 15:17:12 +0530

    use media query for different screen sizes

--- lib/screens/app screens/tasks_screen.dart
+++ lib/screens/app screens/tasks_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/widgets_export.dart';
 
@@ -6,18 +8,26 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //devices screen height and width
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    //defining padding and font size
+
+    final double padding = screenWidth * 0.02;
+    final double titleFontSize = screenWidth * 0.08;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           'Todos',
-          style: AppTextStyles.bold(fontSize: 32),
+          style: AppTextStyles.bold(fontSize: titleFontSize),
         ),
       ),
       floatingActionButton: const FloatingActionButtonWidget(),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
+      body: Padding(
+        padding: EdgeInsets.all(padding),
+        child: const Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             TodaysQuote(),
@@ -28,4 +38,3 @@ class TasksScreen extends StatelessWidget {
     );
   }
 }
-
