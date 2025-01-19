b6a1eada1103ddfbfbb25966bde4d488f71c1df4
1Shibam <hehe.shibam@gmail.com>
2025-01-19 22:35:19 +0530

    fixed text editing controllers

--- lib/providers/controller providers/text_controllers.dart
+++ lib/providers/controller providers/text_controllers.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/providers/provider_exports.dart';
 //text editing controllers for multiple usage
 
-final firstTextController = AutoDisposeStateProvider((ref) {
+final firstTextController = AutoDisposeStateProvider<TextEditingController>((ref) {
   final controller = TextEditingController();
   return controller;
 });
-final secondTextController = AutoDisposeStateProvider((ref) {
+final secondTextController = AutoDisposeStateProvider<TextEditingController>((ref) {
   final controller = TextEditingController();
-  return controller.text;
+  return controller;
 });
-final thirdTextController = AutoDisposeStateProvider((ref) {
+final thirdTextController = AutoDisposeStateProvider<TextEditingController>((ref) {
   final controller = TextEditingController();
-  return controller.text;
+  return controller;
 });
-final fourthTextController = AutoDisposeStateProvider((ref) {
+final fourthTextController = AutoDisposeStateProvider<TextEditingController>((ref) {
   final controller = TextEditingController();
-  return controller.text;
+  return controller;
 });
