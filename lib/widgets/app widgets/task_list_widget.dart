a0955b5066b5fcafc417fdff169c72a7d3a063d2
1Shibam <hehe.shibam@gmail.com>
2025-01-19 23:57:42 +0530

    fixed the list tiles displaying on the scrrenn

--- lib/widgets/app widgets/task_list_widget.dart
+++ lib/widgets/app widgets/task_list_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/widgets_export.dart';
 import '../../providers/provider_exports.dart';
-import 'edit_task_dialog.dart';
 
 class TaskListWidget extends StatelessWidget {
   const TaskListWidget({
@@ -32,59 +31,54 @@ class TaskListWidget extends StatelessWidget {
         }
         //when task is added to the list
         else {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    final task = tasks[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 4.0),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        tileColor: task.isDone!
-                            ? Colors.green.withOpacity(0.3)
-                            : Colors.transparent,
-                        onLongPress: () {
-                          // Show the EditTaskDialog on long press
-                          showDialog(
-                            context: context,
-                            builder: (context) => EditTaskDialog(task: task),
-                          );
-                        },
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Checkbox(
-                              value: task.isDone,
-                              onChanged: (value) {
-                                ref
-                                    .read(taskListProvider.notifier)
-                                    .toggleTaskStatus(task.title);
-                              },
-                            ),
-                            IconButton(
-                                onPressed: () {
-                                  ref
-                                      .read(taskListProvider.notifier)
-                                      .deleteTask(task.title);
-                                },
-                                icon: const Icon(Icons.delete))
-                          ],
+          return Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    tileColor: task.isDone!
+                        ? Colors.green.withOpacity(0.3)
+                        : Colors.lightBlue.withOpacity(0.18),
+                    onLongPress: () {
+                      // Show the EditTaskDialog on long press
+                      showDialog(
+                        context: context,
+                        builder: (context) => EditTaskDialog(task: task),
+                      );
+                    },
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Checkbox(
+                          value: task.isDone,
+                          onChanged: (value) {
+                            ref
+                                .read(taskListProvider.notifier)
+                                .toggleTaskStatus(task.title);
+                          },
                         ),
-                        title: Text(
-                          task.title,
-                          style: AppTextStyles.normal(),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
+                        IconButton(
+                            onPressed: () {
+                              ref
+                                  .read(taskListProvider.notifier)
+                                  .deleteTask(task.title);
+                            },
+                            icon: const Icon(Icons.delete))
+                      ],
+                    ),
+                    title: Text(
+                      task.title,
+                      style: AppTextStyles.normal(),
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         }
       },
