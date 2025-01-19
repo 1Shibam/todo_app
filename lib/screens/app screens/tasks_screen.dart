a61f5f77cdb4ca1d64cb80d37bc98e252436a2fc
1Shibam <hehe.shibam@gmail.com>
2025-01-20 00:03:06 +0530

    wraps in safe area widget

--- lib/screens/app screens/tasks_screen.dart
+++ lib/screens/app screens/tasks_screen.dart
@@ -16,33 +16,35 @@ class TasksScreen extends StatelessWidget {
     final double padding = screenWidth * 0.034;
     final double titleFontSize = screenHeight * 0.04;
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Todos',
-          style: AppTextStyles.bold(fontSize: titleFontSize),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Todos',
+            style: AppTextStyles.bold(fontSize: titleFontSize),
+          ),
         ),
-      ),
-      floatingActionButton: const FloatingActionButtonWidget(),
-      body: Padding(
-        padding: EdgeInsets.all(padding),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const TodaysQuote(),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              'Your task will Appear below - ',
-              style: AppTextStyles.bold(),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const TaskListWidget(),
-          ],
+        floatingActionButton: const FloatingActionButtonWidget(),
+        body: Padding(
+          padding: EdgeInsets.all(padding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const TodaysQuote(),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Your task will Appear below - ',
+                style: AppTextStyles.bold(),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const TaskListWidget(),
+            ],
+          ),
         ),
       ),
     );
