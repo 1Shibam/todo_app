ed09c81750d376dfa1987619299d8878f63eaaa1
1Shibam <hehe.shibam@gmail.com>
2025-01-19 23:57:04 +0530

    sorted exports

--- lib/widgets/widgets_export.dart
+++ lib/widgets/widgets_export.dart
@@ -4,3 +4,4 @@ export 'app widgets/create_task_details_widget.dart';
 export 'app widgets/todays_quote.dart';
 export 'app widgets/floating_action_button_widget.dart';
 export 'app widgets/custom_app_text_field.dart';
+export 'app widgets/edit_task_dialog.dart';
