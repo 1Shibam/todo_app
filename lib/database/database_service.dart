0d6ea7b0bade2113eef62a54407f9e602a7d8a58
1Shibam <hehe.shibam@gmail.com>
2025-01-20 16:50:59 +0530

    created database service usingn sqflite package

---
+++ lib/database/database_service.dart
@@ -0,0 +1,43 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseService {
+  static Database? db;
+  static final DatabaseService instance = DatabaseService._constructor();
+
+  static const String taskTableName = 'tasks'; // Make it a static const
+  static String taskId = 'id';
+  static String taskName = 'title';
+  static String taskStatus = 'isDone';
+  static String taskDesc = 'description';
+
+  DatabaseService._constructor();
+
+  Future<Database> get database async {
+    if (db != null) {
+      return db!;
+    }
+    db = await getDatabase();
+    return db!;
+  }
+
+  Future<Database> getDatabase() async {
+    final databaseDirPath = await getDatabasesPath();
+    final dbPath = join(databaseDirPath, 'main_todo_db.db');
+    final database = await openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $taskTableName (
+            $taskId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $taskName TEXT NOT NULL,
+            $taskDesc TEXT,
+            $taskStatus INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+    return database; // Ensure the database is returned
+  }
+}
