62ae9b1d20119ab143106431f09b55764bd17f63
1Shibam <hehe.shibam@gmail.com>
2025-01-20 14:56:42 +0530

    update the create task list widget

--- lib/widgets/app widgets/create_task_details_widget.dart
+++ lib/widgets/app widgets/create_task_details_widget.dart
@@ -3,37 +3,80 @@ import 'package:todo_app/models/models_exports.dart';
 import 'package:todo_app/providers/provider_exports.dart';
 import '../widgets_export.dart';
 
-class CreateTaskDetailsWidget extends ConsumerWidget {
+class CreateTaskDetailsWidget extends ConsumerStatefulWidget {
   const CreateTaskDetailsWidget({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final taskNameController = ref.watch(firstTextController);
+  ConsumerState<CreateTaskDetailsWidget> createState() =>
+      _CreateTaskDetailsWidgetState();
+}
 
+class _CreateTaskDetailsWidgetState
+    extends ConsumerState<CreateTaskDetailsWidget> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController taskNameController;
+
+  @override
+  void initState() {
+    super.initState();
+    taskNameController = ref.read(firstTextController);
+  }
+
+  void _submitTask() {
+    if (_formKey.currentState!.validate()) {
+      final taskTitle = taskNameController.text.trim();
+
+      // Save input to the state
+      ref.read(taskListProvider.notifier).addTask(
+            TaskDataModel(title: taskTitle),
+          );
+
+      Navigator.pop(context); // Close the dialog
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(
         'Enter Task Details',
         style: AppTextStyles.bold(),
       ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            style: AppTextStyles.normal(),
-            autofocus: true,
-            controller: taskNameController,
-            onSubmitted: (_) {
-              //same as submit button
-              ref.read(firstTextController.notifier).state = taskNameController;
-              final newTask = TaskDataModel(title: taskNameController.text);
-              ref.read(taskListProvider.notifier).addTask(newTask);
-              Navigator.pop(context);
-            },
-            decoration: const InputDecoration(
-                labelText: 'Title', hintText: 'eg. cry more'),
-          )
-        ],
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+                style: AppTextStyles.normal(),
+                autofocus: true,
+                controller: taskNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  hintText: 'e.g. cry more',
+                  errorStyle: TextStyle(color: Colors.red),
+                ),
+                validator: (value) {
+                  final taskTitle = value?.trim() ?? '';
+
+                  if (taskTitle.isEmpty) {
+                    return 'Task title cannot be empty!';
+                  }
+
+                  final isDuplicateTitle = ref
+                      .read(taskListProvider)
+                      .any((task) => task.title == taskTitle);
+                  if (isDuplicateTitle) {
+                    return 'Task with this title already exists!';
+                  }
+
+                  return null; // No validation errors
+                },
+                onFieldSubmitted: (_)=> _submitTask(),
+                ),
+          ],
+        ),
       ),
       actions: [
         TextButton(
@@ -46,14 +89,7 @@ class CreateTaskDetailsWidget extends ConsumerWidget {
           ),
         ),
         ElevatedButton(
-          onPressed: () {
-            // Save input to the state
-            ref.read(firstTextController.notifier).state = taskNameController;
-            final newTask = TaskDataModel(title: taskNameController.text);
-            ref.read(taskListProvider.notifier).addTask(newTask);
-
-            Navigator.pop(context); // Close the dialog
-          },
+          onPressed: _submitTask,
           child: Text(
             'Submit',
             style: AppTextStyles.normal(color: Colors.black, fontSize: 12),
