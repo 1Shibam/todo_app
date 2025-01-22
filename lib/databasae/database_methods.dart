d2000c69cb3efc738993a06bb9e8a4c631b0ffa6
1Shibam <hehe.shibam@gmail.com>
2025-01-23 00:31:18 +0530

    completed insert methods

--- lib/databasae/database_methods.dart
+++ lib/databasae/database_methods.dart
@@ -1,2 +1,13 @@
 //insert a task to database
-Future<void> insertTask(WidgetRef ref, String)
+
+import 'package:sqflite/sqflite.dart';
+import 'package:todo_app/databasae/database.dart';
+import 'package:todo_app/providers/provider_exports.dart';
+
+Future<void> insertTask(WidgetRef ref, String taskTitle,
+    {String description = ''}) async {
+  final db = await ref.read(databaseProvider);
+  await db.insert('tasks',
+      {'title': taskTitle, 'desc': description, 'isDone': 0, 'isDeleted': 0},
+      conflictAlgorithm: ConflictAlgorithm.replace);
+}
