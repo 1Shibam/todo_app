1c4a97caa2ed67134df10e89f1834026a0276148
1Shibam <hehe.shibam@gmail.com>
2025-01-23 01:53:51 +0530

    add method for querying data

--- lib/databasae/database_methods.dart
+++ lib/databasae/database_methods.dart
@@ -1,8 +1,8 @@
-//insert a task to database
-
 import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/databasae/database.dart';
 import 'package:todo_app/providers/provider_exports.dart';
+
+//insert a task to database--
 
 Future<void> insertTask(WidgetRef ref, String taskTitle,
     {String description = ''}) async {
@@ -10,4 +10,14 @@ Future<void> insertTask(WidgetRef ref, String taskTitle,
   await db.insert('tasks',
       {'title': taskTitle, 'desc': description, 'isDone': 0, 'isDeleted': 0},
       conflictAlgorithm: ConflictAlgorithm.replace);
+
+      //checking if the method is running or not
+  print(
+      'data Inserted : $taskTitle is the title and $description is the description added!!');
+}
+
+//Fetching data from the Data base--
+Future<List<Map<String, dynamic>>> getTasks(WidgetRef ref) async {
+  final db = await ref.read(databaseProvider);
+  return await db.query('tasks', where: 'isDeleted = ?', whereArgs: [0]);
 }
