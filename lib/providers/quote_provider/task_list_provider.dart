1a22bd21aa9face8c47ab083bc259e1a3fd5d93e
1Shibam <hehe.shibam@gmail.com>
2025-01-23 01:54:45 +0530

    fixed the out of bound issue and the data is persistent now

---
+++ lib/providers/quote_provider/task_list_provider.dart
@@ -0,0 +1,10 @@
+import 'package:todo_app/databasae/database.dart';
+import 'package:todo_app/providers/provider_exports.dart';
+
+final taskListProvider =
+    FutureProvider<List<Map<String, dynamic>>>((ref) async {
+  final db = await ref.read(databaseProvider);
+  final tasks = await db.query('tasks', where: 'isDeleted = ?', whereArgs: [0]);
+  print('Fetched tasks: $tasks');
+  return tasks;
+});
