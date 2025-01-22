fe50219fd5b160bc13fc41db3c901cf195c3479e
1Shibam <hehe.shibam@gmail.com>
2025-01-23 00:21:54 +0530

    update task model

--- lib/models/data moodels/task_data_model.dart
+++ lib/models/data moodels/task_data_model.dart
@@ -1,13 +1,13 @@
-//task data model class
-// ignore_for_file: must_be_immutable
-
-import 'package:equatable/equatable.dart';
-class TaskDataModel extends Equatable {
+class TaskDataModel {
+  final int id;
   final String title;
+  String? desc;
   bool? isDone;
   bool? isDeleted;
   TaskDataModel({
+    required this.id,
     required this.title,
+    this.desc,
     this.isDone,
     this.isDeleted,
   }) {
@@ -16,12 +16,16 @@ class TaskDataModel extends Equatable {
   }
 
   TaskDataModel copyWith({
+    int? id,
     String? title,
+    String? desc,
     bool? isDone,
     bool? isDeleted,
   }) {
     return TaskDataModel(
+      id: id ?? this.id,
       title: title ?? this.title,
+      desc: desc?? this.desc,
       isDone: isDone ?? this.isDone,
       isDeleted: isDeleted ?? this.isDeleted,
     );
@@ -29,7 +33,9 @@ class TaskDataModel extends Equatable {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id' : id,
       'title': title,
+      'desc' : desc,
       'isDone': isDone,
       'isDeleted': isDeleted,
     };
@@ -37,14 +43,11 @@ class TaskDataModel extends Equatable {
 
   factory TaskDataModel.fromMap(Map<String, dynamic> map) {
     return TaskDataModel(
+      id: map['id'] ?? 0,
       title: map['title'] ?? '',
+      desc: map['desc'] ?? '',
       isDone: map['isDone'] ?? false,
       isDeleted: map['isDeleted'] ?? false,
     );
   }
-
-  @override
-  List<Object?> get props => [
-    title, isDone, isDeleted
-  ];
 }
