e8a801db9d9a1b0cec1fcd2485c711a118d2d451
1Shibam <hehe.shibam@gmail.com>
2025-01-23 15:33:19 +0530

    add deleted task list widget

---
+++ lib/providers/deleted_task_list_provider.dart
@@ -0,0 +1,10 @@
+import 'package:todo_app/databasae/database.dart';
+import 'package:todo_app/providers/provider_exports.dart';
+
+final deletedTaskListProvider =
+    FutureProvider<List<Map<String, dynamic>>>((ref) async {
+  final db = await ref.read(databaseProvider);
+  final tasks = await db.query('tasks'); // Get all tasks to inspect
+  print('All tasks in DB: $tasks'); // Debugging output
+  return tasks.where((task) => task['isDeleted'] == 1).toList();
+});
