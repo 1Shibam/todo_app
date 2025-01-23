ee4bf9996bc31158398a331242c1a22a4753cd08
1Shibam <hehe.shibam@gmail.com>
2025-01-23 15:33:29 +0530

    minor changes

---
+++ lib/providers/task_list_provider.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: avoid_print
+
+import 'package:todo_app/databasae/database.dart';
+import 'package:todo_app/providers/provider_exports.dart';
+
+final taskListProvider =
+    FutureProvider<List<Map<String, dynamic>>>((ref) async {
+  final db = await ref.read(databaseProvider);
+  final tasks = await db.query('tasks', where: 'isDeleted = ?', whereArgs: [0]);
+  print('Fetched tasks: $tasks');
+  return tasks;
+});
+
+
+
