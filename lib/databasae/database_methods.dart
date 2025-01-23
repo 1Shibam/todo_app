2c34ae5158b840ea3a1cfc7ad401c326e538d62e
1Shibam <hehe.shibam@gmail.com>
2025-01-23 12:37:54 +0530

    toggle task status done

--- lib/databasae/database_methods.dart
+++ lib/databasae/database_methods.dart
@@ -3,6 +3,7 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/databasae/database.dart';
 import 'package:todo_app/providers/provider_exports.dart';
+import 'package:todo_app/providers/quote_provider/task_list_provider.dart';
 
 //insert a task to database--
 
@@ -13,7 +14,7 @@ Future<void> insertTask(WidgetRef ref, String taskTitle,
       {'title': taskTitle, 'desc': description, 'isDone': 0, 'isDeleted': 0},
       conflictAlgorithm: ConflictAlgorithm.replace);
 
-      //checking if the method is running or not
+  //checking if the method is running or not
   print(
       'data Inserted : $taskTitle is the title and $description is the description added!!');
 }
@@ -22,4 +23,18 @@ Future<void> insertTask(WidgetRef ref, String taskTitle,
 Future<List<Map<String, dynamic>>> getTasks(WidgetRef ref) async {
   final db = await ref.read(databaseProvider);
   return await db.query('tasks', where: 'isDeleted = ?', whereArgs: [0]);
+}
+
+//toggling task completion status
+Future<void> toggleTaskStatus(WidgetRef ref, int taskId) async {
+  final db = await ref.read(databaseProvider);
+  final task = await db.query('tasks', where: 'id = ?', whereArgs: [taskId]);
+  if (task.isEmpty) {
+    return;
+  }
+  final isDone = task[0]['isDone'] == 1 ? 0 : 1;
+  await db.update('tasks', {'isDone': isDone},
+      where: 'id = ?', whereArgs: [taskId]);
+
+  ref.invalidate(taskListProvider);
 }
