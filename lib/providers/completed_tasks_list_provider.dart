a1f7913d71ebacefd2cb7eb1261fbce09be4e232
1Shibam <hehe.shibam@gmail.com>
2025-01-23 17:46:12 +0530

    fix complete data list not updating!

--- lib/providers/completed_tasks_list_provider.dart
+++ lib/providers/completed_tasks_list_provider.dart
@@ -8,5 +8,5 @@ final completedTasksListProvider =
   final db = await ref.read(databaseProvider);
   final tasks = await db.query('tasks'); // Get all tasks to inspect
   print('All tasks in DB: $tasks'); // Debugging output
-  return tasks.where((task) => task['isDone'] == 1).toList();
+  return tasks.where((task) => task['isDone'] == 1 && task['isDeleted'] == 0).toList();
 });
