811f71e301ccb96ced1a66b8eefc54d56f93ee06
1Shibam <hehe.shibam@gmail.com>
2025-01-31 09:24:57 +0530

    trying cleaning code

---
+++ lib/new Approach/database/database_provider.dart
@@ -0,0 +1,7 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:todo_app/providers/data%20providers/provider_exports.dart';
+
+final databaseProvider = FutureProvider<Database>((ref) async {
+  
+
+});
