33d0d47a376ff28169e20fc67bcaadd5f0548a9f
1Shibam <hehe.shibam@gmail.com>
2025-01-31 09:32:45 +0530

    add database provider

--- lib/new Approach/database/database_provider.dart
+++ lib/new Approach/database/database_provider.dart
@@ -1,7 +1,25 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/providers/data%20providers/provider_exports.dart';
+import 'package:path_provider/path_provider.dart';
 
 final databaseProvider = FutureProvider<Database>((ref) async {
-  
+  final dbPath = await getApplicationDocumentsDirectory();
+  final pathDirectory = join(dbPath.path, 'todos.db');
 
+  return openDatabase(
+    pathDirectory,
+    version: 2,
+    onCreate: (db, version) {
+      '''
+      CREATE TABLE todoTable(
+      todoID INTEGER PRIMARY KEY AUTOINCREMENT,
+      todoTitle TEXT NOT NULL,
+      todoDesc TEXT,
+      todoDate TEXT,
+      todoDone INTEGER DEFAULT 0,
+      todoDeleted INTEGER DEFAULT 0
+      )''';
+    },
+  );
 });
