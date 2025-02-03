f27f1b8ce87de36744f0f3ac148cc21a59e8cce2
1Shibam <hehe.shibam@gmail.com>
2025-02-03 14:50:37 +0530

    floating action widget!!

---
+++ lib/new Approach/UI/widgets/floating_action_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/new%20Approach/app%20widgets/others/app_text_styles.dart';
+
+class FloatingActionWidget extends ConsumerStatefulWidget {
+  const FloatingActionWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _FloatingActionWidgetState();
+}
+
+class _FloatingActionWidgetState extends ConsumerState<FloatingActionWidget> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Cancel',
+                      style: AppTextStyles.normal(),
+                    )),
+                Container(
+                  decoration:
+                      BoxDecoration(color: Colors.lightBlue.withOpacity(0.4)),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Create',
+                        style: AppTextStyles.bold(),
+                      )),
+                )
+              ],
+            );
+          },
+        );
+      },
+      backgroundColor: Colors.grey,
+      child: const Icon(
+        Icons.add_task,
+        size: 32,
+      ),
+    );
+  }
+}
