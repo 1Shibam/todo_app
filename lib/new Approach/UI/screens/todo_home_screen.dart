ecb25d7309f968d8c404cffbe5b330a58a8c338e
1Shibam <hehe.shibam@gmail.com>
2025-02-03 16:37:25 +0530

    fixed tab bar view!

--- lib/new Approach/UI/screens/todo_home_screen.dart
+++ lib/new Approach/UI/screens/todo_home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/new%20Approach/UI/widgets/other_widgets/daily_advice.dart';
 import 'package:todo_app/new%20Approach/themes/app_text_styles.dart';
@@ -24,19 +23,73 @@ class _TodoHomeScreenState extends ConsumerState<TodoHomeScreen> {
           ),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 24,
             ),
             const DailyAdvice(),
-            Text(
-              'Tasks Will show up Below - ',
-              style: AppTextStyles.bold(),
-            ),
             const SizedBox(
               height: 12,
             ),
             
+            DefaultTabController(
+              length: 3, // Three tabs
+              child: Expanded(
+                child: Column(
+                  children: [
+                   
+                    TabBar(
+                      labelStyle: AppTextStyles.bold(fontSize: 16),
+                      indicatorColor: Colors.blue,
+                      dividerColor: Colors.blue.withOpacity(0.8),
+                      unselectedLabelColor: Colors.grey,
+                      labelColor: Colors.blue,
+                      tabs: const [
+                        Tab(
+                          text: 'All Tasks',
+                          icon: Icon(
+                            Icons.task,
+                            size: 32,
+                          ),
+                        ),
+                        Tab(
+                          text: 'Completed',
+                          icon: Icon(
+                            Icons.check,
+                            size: 32,
+                          ),
+                        ),
+                        Tab(
+                          text: 'Deleted',
+                          icon: Icon(
+                            Icons.delete,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+               
+                    const Expanded(
+                      child: TabBarView(
+                        children: [
+                          Center(
+                              child: Text(
+                                  'All Tasks')), 
+                          Center(
+                              child: Text(
+                                  'Completed Tasks')), 
+                          Center(
+                              child: Text(
+                                  'Deleted Tasks')), 
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
