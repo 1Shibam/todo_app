441beb721bd1c87ad0f65650604001d30f08e574
1Shibam <hehe.shibam@gmail.com>
2025-02-03 15:10:31 +0530

    update the home screen!

--- lib/new Approach/UI/screens/todo_home_screen.dart
+++ lib/new Approach/UI/screens/todo_home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todo_app/new%20Approach/UI/widgets/floating_action_widget.dart';
 import 'package:todo_app/new%20Approach/providers/state%20provider/todo_state.dart';
-
-import '../../app widgets/others/app_text_styles.dart';
 
 //! this is the Todo home screen!!
 class TodoHomeScreen extends ConsumerWidget {
@@ -12,33 +9,24 @@ class TodoHomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final todoData = ref.watch(todoListProvider);
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'Todos',
-            style: AppTextStyles.bold(),
-          ),
-        ),
-        floatingActionButton: const FloatingActionWidget(),
-        body: todoData.isEmpty
-            ? Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/empty file.png'),
-                    const Text('NO todos are added yet!!')
-                  ],
-                ),  
-              )
-            : ListView.builder(
-                itemCount: todoData.length,
-                itemBuilder: (context, index) {
-                  final singleData = todoData[index];
-                  return ListTile(
-                    title: Text(singleData.todoTitle),
-                  );
-                },
-              ));
+    return todoData.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/empty file.png'),
+                const Text('NO todos are added yet!!')
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: todoData.length,
+            itemBuilder: (context, index) {
+              final singleData = todoData[index];
+              return ListTile(
+                title: Text(singleData.todoTitle),
+              );
+            },
+          );
   }
 }
