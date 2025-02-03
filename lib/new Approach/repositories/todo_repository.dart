4a4ea30f46bca47101ca512cc3dfe148ecfbeaf1
1Shibam <hehe.shibam@gmail.com>
2025-02-03 15:17:37 +0530

    update the repository for todos!!

--- lib/new Approach/repositories/todo_repository.dart
+++ lib/new Approach/repositories/todo_repository.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/new%20Approach/model/todos_model.dart';
 import 'package:todo_app/new%20Approach/providers/data%20provider/todo_data_source.dart';
 
-
 //! This is the todo repository!!
 class TodoRepository {
   final TodoDataSource dataSource;
@@ -10,6 +9,15 @@ class TodoRepository {
 
   //! Fetch all todos
   Future<List<TodosModel>> fetchTodoList() => dataSource.getTodosList();
+
+  //! Fetch Only Completed Todos
+  Future<List<TodosModel>> fetchOnlyCompletedList() =>
+      dataSource.getCompletedTodosList();
+
+  //! Fetch Soft Deleted Todos
+
+  Future<List<TodosModel>> fetchTemporaryDeletedTodos() =>
+      dataSource.getSoftDeletedTodosList();
 
   //! Add a new todo
   Future<int> insertTodo(TodosModel todo) => dataSource.createTodo(todo);
