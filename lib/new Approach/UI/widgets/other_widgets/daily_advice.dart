6f3c44b357da3fa15539deb5398e2b5eeb6d3c26
1Shibam <hehe.shibam@gmail.com>
2025-02-03 15:19:14 +0530

    cleanup!

--- lib/new Approach/UI/widgets/other_widgets/daily_advice.dart
+++ lib/new Approach/UI/widgets/other_widgets/daily_advice.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../app widgets/others/app_text_styles.dart';
-import '../../providers/state provider/random_quote_provider.dart';
+import '../../../app widgets/others/app_text_styles.dart';
+import '../../../providers/state provider/random_quote_provider.dart';
 
 class TodaysAdvice extends ConsumerWidget {
   const TodaysAdvice({super.key});
