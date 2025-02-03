43851c7b25ed557c38c068562b54d86d2d978c01
1Shibam <hehe.shibam@gmail.com>
2025-02-03 15:39:08 +0530

    app text styles restore!!

---
+++ lib/new Approach/themes/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/new%20Approach/themes/fonts.dart';
+
+
+class AppTextStyles {
+  // Normal font style
+  static TextStyle normal({
+    double fontSize = 20.0,
+    Color color = Colors.black,
+  }) {
+    return TextStyle(
+      fontFamily: font,
+      fontSize: fontSize,
+      color: color,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  // Bold font style
+  static TextStyle bold({
+    double fontSize = 24.0,
+    Color color = Colors.black,
+  }) {
+    return TextStyle(
+      fontFamily: fontBold,
+      fontSize: fontSize,
+      color: color,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
