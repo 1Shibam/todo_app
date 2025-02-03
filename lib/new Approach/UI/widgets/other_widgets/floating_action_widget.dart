2f717556dbc0927463089f326508d370c9fc1725
1Shibam <hehe.shibam@gmail.com>
2025-02-03 16:47:12 +0530

    floating action button for adding new task

--- lib/new Approach/UI/widgets/other_widgets/floating_action_widget.dart
+++ lib/new Approach/UI/widgets/other_widgets/floating_action_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/new%20Approach/themes/app_text_styles.dart';
 
-
 class FloatingActionWidget extends ConsumerStatefulWidget {
   const FloatingActionWidget({super.key});
 
@@ -14,6 +13,8 @@ class FloatingActionWidget extends ConsumerStatefulWidget {
 class _FloatingActionWidgetState extends ConsumerState<FloatingActionWidget> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descController = TextEditingController();
+  final FocusNode titleFocusNode = FocusNode();
+  final FocusNode descFocusNode = FocusNode();
 
   @override
   void dispose() {
@@ -31,25 +32,70 @@ class _FloatingActionWidgetState extends ConsumerState<FloatingActionWidget> {
           context: context,
           builder: (context) {
             return AlertDialog(
+              title: Text(
+                'Enter Task Details',
+                style: AppTextStyles.bold(),
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    style: AppTextStyles.normal(),
+                    controller: titleController,
+                    focusNode: titleFocusNode,
+                    autofocus: true,
+                    onFieldSubmitted: (value) {},
+                    decoration: const InputDecoration(
+                        labelText: 'Title', hintText: 'eg. cry more'),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: descController,
+                    focusNode: descFocusNode,
+                    style: AppTextStyles.normal(),
+                    autofocus: true,
+                    onFieldSubmitted: (value) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Task added to list',
+                            style: AppTextStyles.normal(color: Colors.white)),
+                        duration: const Duration(seconds: 1),
+                      ));
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Description', hintText: 'lol lmao'),
+                  )
+                ],
+              ),
               actions: [
                 TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Cancel',
-                      style: AppTextStyles.normal(),
-                    )),
-                Container(
-                  decoration:
-                      BoxDecoration(color: Colors.lightBlue.withOpacity(0.4)),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Create',
-                        style: AppTextStyles.bold(),
-                      )),
-                )
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog without saving
+                  },
+                  child: Text(
+                    'Cancel',
+                    style:
+                        AppTextStyles.normal(color: Colors.black, fontSize: 12),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Save input to the state
+
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Task added to the database'),
+                      duration: Duration(seconds: 1),
+                    ));
+                  },
+                  child: Text(
+                    'Submit',
+                    style:
+                        AppTextStyles.normal(color: Colors.black, fontSize: 12),
+                  ),
+                ),
               ],
             );
           },
