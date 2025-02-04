c3d4e1f1edd53aca3d5603c0b7a17a2d0ff2cd28
1Shibam <hehe.shibam@gmail.com>
2025-02-04 15:43:54 +0530

    restore task dialog.dart!!

---
+++ lib/new Approach/UI/widgets/dialogs/restore_task_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/new%20Approach/providers/state%20provider/todo_state.dart';
+import 'package:todo_app/new%20Approach/themes/app_text_styles.dart';
+
+class RestoreTaskDialog extends ConsumerWidget {
+  const RestoreTaskDialog({super.key, required this.id});
+  final int id;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AlertDialog(
+      title: Text('Restore Task!?', style: AppTextStyles.bold()),
+      content: RichText(
+          text: TextSpan(
+              text: 'Are you sure you want to restore',
+              style: AppTextStyles.normal(),
+              children: [
+            TextSpan(
+                text: '.',
+                style: AppTextStyles.normal(color: Colors.red).copyWith(
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 3))
+          ])),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel', style: AppTextStyles.normal())),
+        TextButton(
+            onPressed: () {
+              ref.read(todoListProvider.notifier).restoreTodo(id);
+              Navigator.pop(context);
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(4)),
+              child: Text('Delete', style: AppTextStyles.normal()),
+            ))
+      ],
+    );
+  }
+}
