266ade546ce6c84677c0c5436e6fc2881989db40
1Shibam <hehe.shibam@gmail.com>
2025-02-04 19:03:06 +0530

    add deleted todo list widget to show deleted todos and its like a recycle bin can be restore todo from here !!

--- lib/new Approach/UI/widgets/lists_widgets/deleted_todo_list.dart
+++ lib/new Approach/UI/widgets/lists_widgets/deleted_todo_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/new%20Approach/UI/widgets/dialogs/restore_task_dialog.dart';
 import 'package:todo_app/new%20Approach/providers/state%20provider/todo_deleted_state.dart';
-import 'package:todo_app/new%20Approach/providers/state%20provider/todo_state.dart';
+
 import 'package:todo_app/new%20Approach/themes/app_text_styles.dart';
 
 class DeletedTodosList extends ConsumerWidget {
@@ -41,17 +41,6 @@ class DeletedTodosList extends ConsumerWidget {
                   trailing: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Checkbox(
-                        side: const BorderSide(
-                            color: Color.fromARGB(255, 0, 35, 65), width: 2),
-                        activeColor: Colors.blue,
-                        value: singleData.todoCompleted == true,
-                        onChanged: (value) {
-                          ref
-                              .read(todoListProvider.notifier)
-                              .restoreTodo(singleData.todoID!);
-                        },
-                      ),
                       IconButton(
                           onPressed: () {
                             showDialog(
