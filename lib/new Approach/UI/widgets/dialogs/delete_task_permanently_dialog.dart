731e9fedfb5be12f1a76bfab810b39a671f64f67
1Shibam <hehe.shibam@gmail.com>
2025-02-04 19:40:42 +0530

    delete task permanently dialog

---
+++ lib/new Approach/UI/widgets/dialogs/delete_task_permanently_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/new%20Approach/providers/state%20provider/todo_state.dart';
+import 'package:todo_app/new%20Approach/themes/app_text_styles.dart';
+
+class DeleteTaskPermanentlyDialog extends ConsumerWidget {
+  const DeleteTaskPermanentlyDialog({super.key, required this.id});
+  final int id;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AlertDialog(
+      title: Text('Delete Task!?', style: AppTextStyles.bold()),
+      content: Text('Are you sure you want to delete this task permanently!?', style: AppTextStyles.normal(color: Colors.red),),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel', style: AppTextStyles.normal())),
+        TextButton(
+            onPressed: () {
+              ref.read(todoListProvider.notifier).deleteTodo(id);
+              Navigator.pop(context);
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                  color: Colors.red, borderRadius: BorderRadius.circular(4)),
+              child: Text('Delete', style: AppTextStyles.normal()),
+            ))
+      ],
+    );
+  }
+}
