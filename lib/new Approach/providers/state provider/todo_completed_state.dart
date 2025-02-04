4889401e3012134ee18f0d76fba12d9ee7a3fe6b
1Shibam <hehe.shibam@gmail.com>
2025-02-04 19:00:25 +0530

    fixed completed state updation

---
+++ lib/new Approach/providers/state provider/todo_completed_state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:todo_app/new%20Approach/model/todos_model.dart';
+
+import '../../repositories/todo_repository.dart';
+
+final completedTodosListProvider =
+    StateNotifierProvider<TodoCompletedStateNotifier, List<TodosModel>>(
+        (ref) => TodoCompletedStateNotifier(ref));
+
+class TodoCompletedStateNotifier extends StateNotifier<List<TodosModel>> {
+  final Ref ref;
+
+  TodoCompletedStateNotifier(this.ref) : super([]) {
+    loadCompletedTodos();
+  }
+  //! Load completed Todos from the database
+  Future<void> loadCompletedTodos() async {
+    final completeTodos =
+        await ref.read(todoRepositoryProvider).fetchOnlyCompletedList();
+    state = completeTodos;
+  }
+}
