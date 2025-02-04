b9f352c94596fd8c7a96f21813f91fe49900b63a
1Shibam <hehe.shibam@gmail.com>
2025-02-04 19:55:01 +0530

    delete all todo permanently floating action button

---
+++ lib/new Approach/UI/widgets/buttons/delete_all_todos_floating_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../dialogs/delete_all_todos_alert_dialog.dart';
+
+class DeleteAllTodosFloatingButton extends ConsumerWidget {
+  const DeleteAllTodosFloatingButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return DeleteAllTodosAlertDialog();
+          },
+        );
+      },
+      backgroundColor: Colors.red,
+      foregroundColor: Colors.red,
+      child: const Icon(
+        Icons.delete_forever,
+        color: Color.fromARGB(255, 110, 7, 0),
+      ),
+    );
+  }
+}
