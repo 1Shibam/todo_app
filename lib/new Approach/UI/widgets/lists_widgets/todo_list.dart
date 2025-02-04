a3ec5b4f09a4d05cd2cb92857aec7b25b6b3ae62
1Shibam <hehe.shibam@gmail.com>
2025-02-04 13:11:38 +0530

    update todo tile

--- lib/new Approach/UI/widgets/lists_widgets/todo_list.dart
+++ lib/new Approach/UI/widgets/lists_widgets/todo_list.dart
@@ -30,8 +30,42 @@ class TodosList extends ConsumerWidget {
             itemCount: todoData.length,
             itemBuilder: (context, index) {
               final singleData = todoData[index];
-              return ListTile(
-                title: Text(singleData.todoTitle),
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  tileColor: singleData.todoCompleted
+                      ? Colors.lightGreen.withOpacity(0.3)
+                      : Colors.lightBlue.withOpacity(0.3),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Checkbox(
+                        side: const BorderSide(
+                            color: Color.fromARGB(255, 0, 35, 65), width: 2),
+                        activeColor: Colors.blue,
+                        value: singleData.todoCompleted == true,
+                        onChanged: (value) {
+                          ref
+                              .read(todoListProvider.notifier)
+                              .toggleCompletion(singleData);
+                        },
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Color.fromARGB(255, 0, 35, 65),
+                            size: 26,
+                          ))
+                    ],
+                  ),
+                  title: Text(
+                    singleData.todoTitle,
+                    style: AppTextStyles.bold(fontSize: 20),
+                  ),
+                ),
               );
             },
           );
