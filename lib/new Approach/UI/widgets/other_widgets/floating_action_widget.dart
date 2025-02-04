d7972eaa1896828d8ac39be3d58594cb54758269
1Shibam <hehe.shibam@gmail.com>
2025-02-04 11:00:21 +0530

    improving floating action button working!!

--- lib/new Approach/UI/widgets/other_widgets/floating_action_widget.dart
+++ lib/new Approach/UI/widgets/other_widgets/floating_action_widget.dart
@@ -11,17 +11,41 @@ class FloatingActionWidget extends ConsumerStatefulWidget {
 }
 
 class _FloatingActionWidgetState extends ConsumerState<FloatingActionWidget> {
+  //! required text editing controllers
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descController = TextEditingController();
+  final TextEditingController startDateController = TextEditingController();
+  final TextEditingController endDateController = TextEditingController();
+
+  //! focus nodes for text form fields
   final FocusNode titleFocusNode = FocusNode();
   final FocusNode descFocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>(); //! form key for validation..
 
   @override
+  //! getting dispose of controllers when not in use!!
   void dispose() {
     titleController.dispose();
     descController.dispose();
-
+    startDateController.dispose();
+    endDateController.dispose();
     super.dispose();
+  }
+
+  //!method for selecting dates
+
+  Future<void> selectDate(
+      BuildContext context, TextEditingController controller) async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+    if (picked != null) {
+      setState(() {
+        controller.text = picked.toLocal().toString().split('')[0];
+      });
+    }
   }
 
   @override
@@ -36,38 +60,47 @@ class _FloatingActionWidgetState extends ConsumerState<FloatingActionWidget> {
                 'Enter Task Details',
                 style: AppTextStyles.bold(),
               ),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    style: AppTextStyles.normal(),
-                    controller: titleController,
-                    focusNode: titleFocusNode,
-                    autofocus: true,
-                    onFieldSubmitted: (value) {},
-                    decoration: const InputDecoration(
-                        labelText: 'Title', hintText: 'eg. cry more'),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    controller: descController,
-                    focusNode: descFocusNode,
-                    style: AppTextStyles.normal(),
-                    autofocus: true,
-                    onFieldSubmitted: (value) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('Task added to list',
-                            style: AppTextStyles.normal(color: Colors.white)),
-                        duration: const Duration(seconds: 1),
-                      ));
-                    },
-                    decoration: const InputDecoration(
-                        labelText: 'Description', hintText: 'lol lmao'),
-                  )
-                ],
+              content: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      style: AppTextStyles.normal(),
+                      controller: titleController,
+                      focusNode: titleFocusNode,
+                      autofocus: true,
+                      onFieldSubmitted: (value) {},
+                      decoration: const InputDecoration(
+                          labelText: 'Title', hintText: 'eg. cry more'),
+                          validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Title cannot be empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: descController,
+                      focusNode: descFocusNode,
+                      style: AppTextStyles.normal(),
+                      autofocus: true,
+                      onFieldSubmitted: (value) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('Task added to list',
+                              style: AppTextStyles.normal(color: Colors.white)),
+                          duration: const Duration(seconds: 1),
+                        ));
+                      },
+                      decoration: const InputDecoration(
+                          labelText: 'Description', hintText: 'lol lmao'),
+                    )
+                  ],
+                ),
               ),
               actions: [
                 TextButton(
